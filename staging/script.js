c01fe904b05b7703a3d386493361d316021c0277
Travis Dodson <travisrdodson@gmail.com>
2020-08-14 20:50:48 -0500

    updates

--- staging/script.js
+++ staging/script.js
@@ -7,8 +7,8 @@ function convertToHBCB(e){
   }else{
     result.innerHTML = 'You\'ve got at least ' + divide(amount) + ' Honey Butter Chicken Biscuits! <br> <form action="https://whataburger.com/ordering/startorder"><input class="cta-select-form" type="submit" value="ORDER NOW" /><br><br></form>';
   }
-/*   var parent    = document.getElementById('images'),
-    imagePath = 'https://staging.howmanyhoneybutterchickenbiscuits.com/img/HoneyButterChickenBiscuitsmall.png',
+  var parent    = document.getElementById('images'),
+    imagePath = './img/HoneyButterChickenBiscuitsmall.png',
     img;
 if (parent.children.length > 0) {
   for (i = 0; i < parent.children.length; i = i){parent.children[0].remove()}
@@ -19,7 +19,7 @@ for (var i = 0; i < divide(amount); i++) {
     img.src = imagePath;
 
     parent.appendChild(img);
-} */
+}
   clearValue();
 }
 
